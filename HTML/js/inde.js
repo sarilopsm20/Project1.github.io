c6d9312be95e0c2866dfc89788a00b30dc994538
sarilopsm20 <44045585+sarilopsm20@users.noreply.github.com>
2023-07-30 16:49:19 -0500

    main

--- HTML/js/inde.js
+++ HTML/js/inde.js
@@ -1,16 +1,1 @@
-//Create a program to get FizzBuzz when the position is 
-
-var list = new Array ()
-
-for (var i = 0; i <= 10; i++){
-    if (i%3 == 0 && i%5==0){
-        list.push("FizzBuzz")
-    } else if (i%3==0){
-        list.push("Fizz")
-    } else if (i%5==0){
-        list.push("Buzz")
-    }else{
-        list.push(i)
-    }
-}ÿ
-document.write(list)
+ff
